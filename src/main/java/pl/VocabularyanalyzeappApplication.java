06958e8144f0850eebdcf96e79abe1b3a9c41f85
roboblog <Robovlogg@gmail.com>
2017-08-16 22:05:28 +0200

    Initial commit

---
+++ src/main/java/pl/VocabularyanalyzeappApplication.java
@@ -0,0 +1,12 @@
+package pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyanalyzeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyanalyzeappApplication.class, args);
+	}
+}
