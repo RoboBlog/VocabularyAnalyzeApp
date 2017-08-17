1768e93177d4f4fafb1e3066f2733b3c817bccef
roboblog <Robovlogg@gmail.com>
2017-08-17 02:09:39 +0200

    add file upload

--- src/main/java/pl/VocabularyanalyzeappApplication.java
+++ src/main/java/pl/VocabularyanalyzeappApplication.java
@@ -1,12 +1,26 @@
 package pl;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import pl.fileupload.StorageProperties;
+import pl.fileupload.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class VocabularyanalyzeappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VocabularyanalyzeappApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
---
+++ src/main/java/pl/fileupload/FileUploadController.java
@@ -0,0 +1,43 @@
+package pl.fileupload;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+
+    @PostMapping("/up")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        storageService.store(file);
+
+        return "You successfully uploaded " + file.getOriginalFilename() + "!";
+    }
+
+
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
